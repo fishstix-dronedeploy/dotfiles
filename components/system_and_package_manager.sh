7120a5eb63b7f9e35f7f283d33c391d9c46abd35
Scott Fisher <scott.fisher@dronedeploy.com>
2025-06-11 10:50:31 -0700

    change how docker is installed

--- components/system_and_package_manager.sh
+++ components/system_and_package_manager.sh
@@ -110,6 +110,7 @@ install_programs() {
           exit 1
         fi
       done
+      brew cask install docker
       ;;
     pkg)
       _process "Installing dependencies using pkg package manager"
